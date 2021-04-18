5d9f566dd92a003926d662e23e13f424b3df7410
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-04-18 11:06:24 -0300

    SPOCAN-21 elimino espacio en local.properties. Agrego constructor protegido en MockedBackend. Cambio int por Integer por posibles nulos httpStatus de Fallible

--- app/src/main/java/com/neiapp/spocan/backend/MockedBackend.java
+++ app/src/main/java/com/neiapp/spocan/backend/MockedBackend.java
@@ -1,6 +1,5 @@
 package com.neiapp.spocan.backend;
 
-import com.neiapp.spocan.backend.Backend;
-
 public class MockedBackend implements Backend {
+    protected MockedBackend(){}
 }
--- app/src/main/java/com/neiapp/spocan/backend/callback/Fallible.java
+++ app/src/main/java/com/neiapp/spocan/backend/callback/Fallible.java
@@ -1,7 +1,7 @@
 package com.neiapp.spocan.backend.callback;
 
 public interface Fallible {
-    default void onFailure(String message, int httpStatus) {
+    default void onFailure(String message, Integer httpStatus) {
         System.out.println(String.format("An error has occurred. Message: %s - Status: %s", message, httpStatus));
     }
 }
