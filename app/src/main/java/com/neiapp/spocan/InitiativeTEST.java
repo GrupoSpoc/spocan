767acb4e20e48e1b074b9ce0e2b5b0684348d7e7
gaston <gaston.92@hotmail.com.ar>
2021-04-18 21:28:54 -0300

    spocan-10 test

---
+++ app/src/main/java/com/neiapp/spocan/InitiativeTEST.java
@@ -0,0 +1,18 @@
+package com.neiapp.spocan;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InitiativeTEST extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initiative);
+        Intent intent = getIntent();
+
+    }
+
+}
