b59a35a92a10eda4762dd77d4a5c0a04273e39f1
lPretti <lprettimolina@gmail.com>
2021-04-18 21:04:08 -0300

    SPOCAN-19 agrego: carpeta models, clase initiative, dependencia gson

---
+++ app/src/main/java/com/neiapp/spocan/Models/Initiative.java
@@ -0,0 +1,67 @@
+package com.neiapp.spocan.Models;
+
+import com.google.gson.Gson;
+
+public class Initiative {
+
+    private String id;
+    private String title;
+    private String description;
+    private String imageBase64;
+    private String nickName;
+    private boolean isFromCurrentUser;
+
+    public Initiative(String id, String title, String description, String imageBase64, String nickName, boolean isFromCurrentUser) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imageBase64 = imageBase64;
+        this.nickName = nickName;
+        this.isFromCurrentUser = isFromCurrentUser;
+    }
+
+    public Initiative(String title, String description, String imageBase64, boolean isFromCurrentUser) {
+        this.title = title;
+        this.description = description;
+        this.imageBase64 = imageBase64;
+        this.isFromCurrentUser = isFromCurrentUser;
+        this.id = null;
+        this.nickName = null;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageBase64() {
+        return imageBase64;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public boolean isFromCurrentUser() {
+        return isFromCurrentUser;
+    }
+
+     public String toJson(Initiative initiativeToTransform){
+     Gson gson = new Gson();
+     String jsonResult = gson.toJson(initiativeToTransform);
+
+        return jsonResult;
+     }
+     public  static Initiative convertJson (String jsonToTransform){
+       Gson gson = new Gson();
+       Initiative initiativeResulting =  (Initiative) gson.fromJson(jsonToTransform, Initiative.class);
+        return initiativeResulting;
+     }
+
+     }
