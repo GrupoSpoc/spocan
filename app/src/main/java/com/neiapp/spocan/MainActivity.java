742c9d1b54724143f81161afe0743d1610e65357
gaston <gaston.92@hotmail.com.ar>
2021-04-23 23:14:47 -0300

    Probablemente a borrar

--- app/src/main/java/com/neiapp/spocan/MainActivity.java
+++ app/src/main/java/com/neiapp/spocan/MainActivity.java
@@ -2,13 +2,28 @@ package com.neiapp.spocan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mGoToCreateInitiativeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoToCreateInitiativeBtn = findViewById(R.id.HomeButton);
+
+        mGoToCreateInitiativeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), InitiativeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
