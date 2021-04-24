074c5732849c7fd93544173837f753c7c423238a
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-04-24 11:41:45 -0300

    SPOCAN-25 Agrego dependencia de Roboelectric para UnitTest. Creo clase helper para convertir bitmap a base64 y viceversa. Testeo ambos métodos

---
+++ app/src/main/java/com/neiapp/spocan/util/Base64Converter.java
@@ -0,0 +1,21 @@
+package com.neiapp.spocan.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class Base64Converter {
+    public static String bitmapToBase64(Bitmap mBitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        mBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    public static Bitmap base64ToBitmap(String base64) {
+        byte[] byteArray = Base64.decode(base64.getBytes(), Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+    }
+}
