32c2436906977059cff9a5e3c6c37177ca808e0b
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-04-24 11:53:15 -0300

    SPOCAN-25 borro import en desuso

--- app/src/test/java/com/neiapp/spocan/Models/InitiativeTest.java
+++ app/src/test/java/com/neiapp/spocan/Models/InitiativeTest.java
@@ -1,19 +1,18 @@
  package com.neiapp.spocan.Models;
 
-import android.graphics.Bitmap;
+ import android.graphics.Bitmap;
 
-import com.google.gson.JsonObject;
+ import com.google.gson.JsonObject;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+ import org.junit.Test;
+ import org.junit.runner.RunWith;
+ import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+ import static org.junit.Assert.assertEquals;
+ import static org.junit.Assert.assertFalse;
+ import static org.junit.Assert.assertNotNull;
+ import static org.junit.Assert.assertNull;
+ import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 public class InitiativeTest {
