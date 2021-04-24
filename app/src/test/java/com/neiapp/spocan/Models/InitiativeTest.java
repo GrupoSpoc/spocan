2dc6e45813b38285cc2aec66084fa0e99a76eb01
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-04-24 11:52:23 -0300

    SPOCAN-25 agrego getter y setter de image con bitmap en Initiative, consumiendo los servicios del conversor. Agrego test y corrijo indentaciones

--- app/src/test/java/com/neiapp/spocan/Models/InitiativeTest.java
+++ app/src/test/java/com/neiapp/spocan/Models/InitiativeTest.java
@@ -1,10 +1,21 @@
  package com.neiapp.spocan.Models;
 
+import android.graphics.Bitmap;
+
 import com.google.gson.JsonObject;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
 public class InitiativeTest {
     @Test
     public void testInitiativeCreation(){
@@ -15,17 +26,16 @@ public class InitiativeTest {
 
         Initiative initiative = new Initiative(title, description, imageBase64, isFromCurrentUser);
 
-        Assert.assertEquals(title, initiative.getTitle());
-        Assert.assertEquals(description, initiative.getDescription());
-        Assert.assertEquals(imageBase64, initiative.getImageBase64());
-        Assert.assertFalse(initiative.isFromCurrentUser());
-        Assert.assertNull(initiative.getId());
-        Assert.assertNull(initiative.getNickName());
-
+        assertEquals(title, initiative.getTitle());
+        assertEquals(description, initiative.getDescription());
+        assertEquals(imageBase64, initiative.getImageBase64());
+        assertFalse(initiative.isFromCurrentUser());
+        assertNull(initiative.getId());
+        assertNull(initiative.getNickName());
     }
 
     @Test
-   public void testInitiativeCreationWithout(){
+    public void testInitiativeCreationWithout(){
         String  title = "title";
         String  description = "description";
         String  imageBase64 = "imageBase64";
@@ -35,13 +45,12 @@ public class InitiativeTest {
 
         Initiative initiative = new Initiative(id,title, description, imageBase64, nickname,isFromCurrentUser);
 
-        Assert.assertEquals(title, initiative.getTitle());
-        Assert.assertEquals(description, initiative.getDescription());
-        Assert.assertEquals(imageBase64, initiative.getImageBase64());
-        Assert.assertTrue(initiative.isFromCurrentUser());
-        Assert.assertEquals(id, initiative.getId());
-        Assert.assertEquals(nickname, initiative.getNickName());
-
+        assertEquals(title, initiative.getTitle());
+        assertEquals(description, initiative.getDescription());
+        assertEquals(imageBase64, initiative.getImageBase64());
+        assertTrue(initiative.isFromCurrentUser());
+        assertEquals(id, initiative.getId());
+        assertEquals(nickname, initiative.getNickName());
     }
 
     @Test
@@ -58,14 +67,12 @@ public class InitiativeTest {
 
         Initiative result = Initiative.convertJson(jsonInitiative.toString());
 
-        Assert.assertEquals(result.getDescription(), jsonInitiative.get("description").getAsString());
-        Assert.assertEquals(result.getId(), jsonInitiative.get("id").getAsString());
-        Assert.assertEquals(result.getImageBase64(), jsonInitiative.get("imageBase64").getAsString());
-        Assert.assertEquals(result.getNickName(), jsonInitiative.get("nickName").getAsString());
-        Assert.assertEquals(result.getTitle(), jsonInitiative.get("title").getAsString());
-        Assert.assertFalse(result.isFromCurrentUser());
-
-
+        assertEquals(result.getDescription(), jsonInitiative.get("description").getAsString());
+        assertEquals(result.getId(), jsonInitiative.get("id").getAsString());
+        assertEquals(result.getImageBase64(), jsonInitiative.get("imageBase64").getAsString());
+        assertEquals(result.getNickName(), jsonInitiative.get("nickName").getAsString());
+        assertEquals(result.getTitle(), jsonInitiative.get("title").getAsString());
+        assertFalse(result.isFromCurrentUser());
     }
 
     @Test
@@ -82,12 +89,47 @@ public class InitiativeTest {
         String jsonInitiative = initiative.toJson();
         Initiative result = Initiative.convertJson(jsonInitiative);
 
-        Assert.assertEquals(title, result.getTitle());
-        Assert.assertEquals(description, result.getDescription());
-        Assert.assertEquals(imageBase64, result.getImageBase64());
-        Assert.assertTrue(result.isFromCurrentUser());
-        Assert.assertEquals(id, result.getId());
-        Assert.assertEquals(nickname, result.getNickName());
+        assertEquals(title, result.getTitle());
+        assertEquals(description, result.getDescription());
+        assertEquals(imageBase64, result.getImageBase64());
+        assertTrue(result.isFromCurrentUser());
+        assertEquals(id, result.getId());
+        assertEquals(nickname, result.getNickName());
     }
 
+    @Test
+    public void testBuildInitiativeWithImage() {
+        String  title = "title";
+        String  description = "description";
+        boolean isFromCurrentUser = true;
+        final Bitmap bitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ALPHA_8);
+
+        Initiative initiative = new Initiative(title, description, bitmap, isFromCurrentUser);
+
+        assertNotNull(initiative.getImageBase64());
+        assertNotNull(initiative.getImage());
+        assertEquals(title, initiative.getTitle());
+        assertEquals(description, initiative.getDescription());
+        assertTrue(initiative.isFromCurrentUser());
+        assertNull(initiative.getId());
+        assertNull(initiative.getNickName());
+    }
+
+    @Test
+    public void testSettingAndGettingImage() {
+        String  title = "title";
+        String  description = "description";
+        String  imageBase64 = null;
+        boolean isFromCurrentUser = true;
+        String nickname = "lolo";
+        String id= "id";
+        final Bitmap bitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ALPHA_8);
+
+        Initiative initiative = new Initiative(id,title, description, imageBase64, nickname,isFromCurrentUser);
+
+        initiative.setImage(bitmap);
+
+        assertNotNull(initiative.getImageBase64());
+        assertNotNull(initiative.getImage());
+    }
 }
