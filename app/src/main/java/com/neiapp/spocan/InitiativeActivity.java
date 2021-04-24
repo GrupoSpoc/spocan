d5ffa9e6bf56f372c6d07f3f4966a1d41075975d
gaston <gaston.92@hotmail.com.ar>
2021-04-23 22:13:38 -0300

    Cambios extra para el build

--- app/src/main/java/com/neiapp/spocan/InitiativeActivity.java
+++ app/src/main/java/com/neiapp/spocan/InitiativeActivity.java
@@ -2,17 +2,48 @@ package com.neiapp.spocan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InitiativeTEST extends AppCompatActivity {
+public class InitiativeActivity extends AppCompatActivity {
+
+    Button mAddPhotoBtn;
+    Button mPublishInitiativeBtn;
+    Button mCancelPublishBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initiative);
-        Intent intent = getIntent();
 
+        mAddPhotoBtn = findViewById(R.id.addPhotoBtn);
+        mCancelPublishBtn = findViewById(R.id.cancel_button);
+        mPublishInitiativeBtn = findViewById(R.id.publish_button);
+
+
+        mAddPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d( "addPhoto", "Aca se deberia pasar a la pantalla que agrega la foto");
+            }
+        });
+
+        mCancelPublishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d( "cancel", "Aca se deberia volver al home o desde donde se hubiera llamado a agregar iniciativa");
+            }
+        });
+
+        mPublishInitiativeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d( "publish", "Aca se deberia mostrar un toast con _operacion completada_ y redirigirse al home/muro");
+            }
+        });
     }
 
 }
