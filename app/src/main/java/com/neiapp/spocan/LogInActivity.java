2c2d0da8de2d9d645c7ac8625087e971d7bc58ce
gaston <gaston.92@hotmail.com.ar>
2021-04-25 23:04:39 -0300

    Segundo commit, se arregla diseño y tamaños

--- app/src/main/java/com/neiapp/spocan/LogInActivity.java
+++ app/src/main/java/com/neiapp/spocan/LogInActivity.java
@@ -3,6 +3,7 @@ package com.neiapp.spocan;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,25 +11,14 @@ import androidx.annotation.Nullable;
 
 public class LogInActivity extends Activity {
 
-    Button mRegularlogInBtn, mSignInWithGoogleBtn, mRegisterBtn;
+    Button mSignInWithGoogleBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_user);
 
-        mRegularlogInBtn.findViewById(R.id.regular_login_btn);
         mSignInWithGoogleBtn.findViewById(R.id.google_login_btn);
-        mRegisterBtn.findViewById(R.id.register_btn);
-
-
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               // Intent intent =  new Intent(getApplicationContext(), RegisterUserActivity.class);
-                //startActivity(intent);
-            }
-        });
 
     }
 }
