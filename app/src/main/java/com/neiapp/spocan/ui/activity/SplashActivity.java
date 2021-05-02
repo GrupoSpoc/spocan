8b9e27ad8bcbab4d8fe3972e602a9e5a48100926
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-05-02 01:14:53 -0300

    SPOCAN-62 borro imports en desuso

--- app/src/main/java/com/neiapp/spocan/ui/activity/SplashActivity.java
+++ app/src/main/java/com/neiapp/spocan/ui/activity/SplashActivity.java
@@ -3,17 +3,13 @@ package com.neiapp.spocan.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
-
-import android.os.Handler;
 
 import com.neiapp.spocan.BuildConfig;
 import com.neiapp.spocan.R;
 import com.neiapp.spocan.backend.Backend;
-import com.neiapp.spocan.backend.callback.CallbackInstance;
-import com.neiapp.spocan.backend.callback.CallbackVoid;
 
 
 public class SplashActivity extends Activity {
@@ -48,7 +44,7 @@ public class SplashActivity extends Activity {
         handler.postDelayed(() -> {
             final Intent intent;
 
-            if (Boolean.parseBoolean(BuildConfig.test)) {
+            if (Backend.test()) {
                 intent = new Intent(SplashActivity.this, MainActivity.class);
             } else {
                 intent = new Intent(SplashActivity.this, LoginActivity.class);
