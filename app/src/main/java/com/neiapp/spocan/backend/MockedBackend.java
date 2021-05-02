3561b0447d3d7ea2efe3e5a84640753e50f8a7f7
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-05-01 23:53:35 -0300

    SPOCAN-62 agrego authenticate() para obtener usuario y token

--- app/src/main/java/com/neiapp/spocan/backend/MockedBackend.java
+++ app/src/main/java/com/neiapp/spocan/backend/MockedBackend.java
@@ -1,7 +1,10 @@
 package com.neiapp.spocan.backend;
 
 import com.neiapp.spocan.Models.Initiative;
+import com.neiapp.spocan.Models.User;
+import com.neiapp.spocan.Models.UserType;
 import com.neiapp.spocan.backend.callback.CallbackCollection;
+import com.neiapp.spocan.backend.callback.CallbackInstance;
 import com.neiapp.spocan.backend.callback.CallbackVoid;
 
 
@@ -9,15 +12,20 @@ import java.util.ArrayList;
 
 public class MockedBackend implements Backend {
     public static ArrayList<Initiative> initiative_store;
+    public static final User USER = new User("CurrentUser", UserType.PERSON);
 
     protected MockedBackend() {
         if (initiative_store == null) {
             initiative_store = new ArrayList<>();
             initiative_store.add(new Initiative("1", "[test] Iniciativa de prueba 1", MockedImagesBase64.RED_800_X_600, "TestUser-1", "2021-04-27T22:59:34", false));
             initiative_store.add(new Initiative("2", "[test] Iniciativa de prueba 2. Esta vez la descripcion es un poco mas larga.", MockedImagesBase64.BLUE_800_X_600, "TestUser-2", "2021-04-22T14:13:05", false));
-            initiative_store.add(new Initiative("3", "[test] Iniciativa de prueba 3", MockedImagesBase64.GREEN_800_X_600, "TestUser-Current", "2021-03-16T18:07:14", true));
+            initiative_store.add(new Initiative("3", "[test] Iniciativa de prueba 3", MockedImagesBase64.GREEN_800_X_600, "CurrentUser", "2021-03-16T18:07:14", true));
             initiative_store.add(new Initiative("4", "[test] Iniciativa de prueba 4", MockedImagesBase64.YELLOW_800_X_600, "TestUser-1", "2020-09-11T11:42:35", false));
         }
+    }
+
+    public static void doAuthenticate(CallbackInstance<User> callback) {
+        callback.onSuccess(USER);
     }
 
     @Override
@@ -33,7 +41,7 @@ public class MockedBackend implements Backend {
         collection.onSuccess(initiative_store);
     }
 
-    public static void resetStorage(){
-        initiative_store.clear();
+    public static void resetStorage() {
+        if (initiative_store != null) initiative_store.clear();
     }
 }
--- app/src/main/java/com/neiapp/spocan/backend/RestClientBackend.java
+++ app/src/main/java/com/neiapp/spocan/backend/RestClientBackend.java
@@ -1,9 +1,34 @@
 package com.neiapp.spocan.backend;
 
-public class RestClientBackend { // todo implementar Backend
-    private final String token;
+import com.neiapp.spocan.Models.Initiative;
+import com.neiapp.spocan.Models.User;
+import com.neiapp.spocan.backend.callback.CallbackCollection;
+import com.neiapp.spocan.backend.callback.CallbackInstance;
+import com.neiapp.spocan.backend.callback.CallbackVoid;
 
-    protected RestClientBackend(String token) {
-        this.token = token;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestClientBackend implements Backend { // todo implementar Backend
+    private static String jwt;
+
+
+    public RestClientBackend() {
+
+    }
+
+    public static void doAuthenticate(String firebaseToken, CallbackInstance<User> callback) {
+
+    }
+
+    @Override
+    public void createInitiative(Initiative initiative, CallbackVoid callback) {
+
+    }
+
+    @Override
+    public void getAll(CallbackCollection<Initiative> callback) {
+        final List<Initiative> initiativeList = new ArrayList<>(); // hacer pegada al backend para obtener initiatives
+        callback.onSuccess(initiativeList);
     }
 }
