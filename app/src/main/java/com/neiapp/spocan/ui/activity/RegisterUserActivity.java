68fd815cad89a4120daf03eb04c22db2c23a5263
gaston <gaston.92@hotmail.com.ar>
2021-05-04 14:45:27 -0300

    Se modifican las lineas discutidas en el PR para este branch

--- app/src/main/java/com/neiapp/spocan/ui/activity/RegisterUserActivity.java
+++ app/src/main/java/com/neiapp/spocan/ui/activity/RegisterUserActivity.java
@@ -23,9 +23,7 @@ import org.w3c.dom.Text;
 public class RegisterUserActivity extends Activity {
 
     private Spinner spinner;
-    private UserType type;
-    private UserType otherType;
-    private String selectedType;
+    private UserType selectedType;
     private Button registerButton;
     private TextView userNicknameInput;
     private CharSequence userNickname;
@@ -34,6 +32,8 @@ public class RegisterUserActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_user);
+        userNicknameInput = findViewById(R.id.userTextBox);
+        registerButton = findViewById(R.id.registerButton);
 
         //Construccion del dropdown para seleccionar tipo de usuario
         spinner = findViewById(R.id.userTypeSpinner);
@@ -41,30 +41,23 @@ public class RegisterUserActivity extends Activity {
         ArrayAdapter<UserType> adapter = new ArrayAdapter<UserType>(this, android.R.layout.simple_spinner_dropdown_item, userTypeOptions);
         spinner.setAdapter(adapter);
 
-        //Obtener nickname del input
-        userNicknameInput = findViewById(R.id.userTextBox);
-        userNickname = userNicknameInput.getText();
-
-        //Obtener tipo seleccionado
-        selectedType = spinner.getSelectedItem().toString();
-        otherType = UserType.valueOf(selectedType);
-
-        registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Obtener nickname del input
+                userNickname = userNicknameInput.getText();
+                //Obtener tipo seleccionado
+                selectedType = (UserType) spinner.getSelectedItem();
                 //TODO: Acá pasarle al BE los datos del form para registrar al usuario nuevo (nickname del input y la opcion seleccionada del dropdown)
-                if (!userNickname.toString().isEmpty() && otherType != null) {
+                if (!userNickname.toString().isEmpty()) {
 
-                    Log.d("valid user data", "Datos bien enviados");
-                    Toast.makeText(getApplicationContext(), "usuario: " + userNickname + " tipo: " + otherType, Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "usuario: " + userNickname + " tipo: " + selectedType, Toast.LENGTH_LONG).show();
 
-                    //TODO: Despues tiene que redirigir al main activity
+                    //TODO: Redirigir al main activity
                     //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     //startActivity(intent);
                 } else {
-                    //Codigo si alguno de los datos no esta ingresado/seleccionado
-                    Toast.makeText(getApplicationContext(), "Ingrese un usuario para continuar", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Debe ingresar un usuario válido para continuar", Toast.LENGTH_SHORT).show();
                 }
             }
         });
