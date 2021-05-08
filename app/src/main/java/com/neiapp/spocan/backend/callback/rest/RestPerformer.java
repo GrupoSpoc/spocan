e73c9be13edf710dffff1ef58ee44686ea22b8f8
Nicolas Ortola <nicolas.ortola@mercadolibre.com>
2021-05-08 10:54:54 -0300

    SPOCAN-59 ajusto RestPerformer para enviar texto plano y respetar convencions de headers del backend

--- app/src/main/java/com/neiapp/spocan/backend/callback/rest/RestPerformer.java
+++ app/src/main/java/com/neiapp/spocan/backend/callback/rest/RestPerformer.java
@@ -7,8 +7,10 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 
 public class RestPerformer {
-    private static final String USER_TOKEN_HEADER = "User-Token";
-    private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String CHARSET_UTF_8 = "charset=utf-8";
+    private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; " + CHARSET_UTF_8;
+    private static final String TEXT_PLAIN_CHARSET_UTF_8 = "text/plain; " + CHARSET_UTF_8;
 
     private final OkHttpClient httpClient;
     private final String jwt;
@@ -25,7 +27,12 @@ public class RestPerformer {
     }
 
     public void post(String url, String payload, Callback serverCallback) {
-        Request request = buildPostRequest(url, payload);
+        Request request = buildPostRequestJson(url, payload);
+        perform(request, serverCallback);
+    }
+
+    public void postText(String url, String payload, Callback serverCallback) {
+        Request request = buildPostRequestJson(url, payload);
         perform(request, serverCallback);
     }
 
@@ -34,18 +41,30 @@ public class RestPerformer {
     }
 
     private Request buildGetRequest(String url) {
-        return new Request.Builder()
-                .url(url)
+        return commonRequestBuilder(url)
                 .get()
-                .addHeader(USER_TOKEN_HEADER, jwt)
                 .build();
     }
 
-    private Request buildPostRequest(String url, String payload) {
+    private Request buildPostRequestJson(String url, String payload) {
+        return commonRequestBuilder(url)
+                .post(RequestBody.create(MediaType.parse(APPLICATION_JSON_CHARSET_UTF_8), payload))
+                .build();
+    }
+
+    private Request buildPostRequestText(String url, String payload) {
+        return buildPostRequest(url, payload, MediaType.parse(TEXT_PLAIN_CHARSET_UTF_8));
+    }
+
+    private Request buildPostRequest(String url, String payload, MediaType mediaType) {
+        return commonRequestBuilder(url)
+                .post(RequestBody.create(mediaType, payload))
+                .build();
+    }
+
+    private Request.Builder commonRequestBuilder(String url) {
         return new Request.Builder()
                 .url(url)
-                .post(RequestBody.create(MediaType.parse(APPLICATION_JSON_CHARSET_UTF_8), payload))
-                .addHeader(USER_TOKEN_HEADER, jwt)
-                .build();
+                .addHeader(AUTHORIZATION_HEADER, "Bearer " + jwt);
     }
 }
