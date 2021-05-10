82a8219546a6a5292b3cb6e7df30e456e8f1d306
lPretti <lprettimolina@gmail.com>
2021-05-09 22:28:11 -0300

    SPOCAN-30 Agrego método para validar la creación de la iniciativa

--- app/src/main/java/com/neiapp/spocan/ui/activity/InitiativeActivity.java
+++ app/src/main/java/com/neiapp/spocan/ui/activity/InitiativeActivity.java
@@ -12,16 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-
 import com.neiapp.spocan.Models.Initiative;
 import com.neiapp.spocan.R;
 import com.neiapp.spocan.backend.Backend;
 import com.neiapp.spocan.backend.callback.CallbackVoid;
-
 import java.util.Objects;
 
 public class InitiativeActivity extends AppCompatActivity {
@@ -33,7 +30,6 @@ public class InitiativeActivity extends AppCompatActivity {
     EditText textDescription;
     Bitmap bitmap = null;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,9 +40,6 @@ public class InitiativeActivity extends AppCompatActivity {
         mPublishInitiativeBtn = findViewById(R.id.publish_button);
         textDescription = findViewById(R.id.editTextDescription);
         imageView = findViewById((R.id.imageView));
-        Toast toast_photoError = Toast.makeText(this, "DEBE AGREGAR UNA FOTO", Toast.LENGTH_LONG);
-        Toast toast_descriptionError = Toast.makeText(this, "DEBE AGREGAR UNA DESCRIPCION", Toast.LENGTH_LONG);
-        Toast toast_succsess = Toast.makeText(this, "EXITO", Toast.LENGTH_LONG);
 
         mAddPhotoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +54,7 @@ public class InitiativeActivity extends AppCompatActivity {
                 }
             }
         });
+
         mCancelPublishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,32 +66,38 @@ public class InitiativeActivity extends AppCompatActivity {
         mPublishInitiativeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (bitmap != null) {
-                    if (!TextUtils.isEmpty(textDescription.getText())) {
-                        Initiative intitiative = new Initiative(textDescription.getText().toString(), bitmap, true);
-                        Backend backend = Backend.getInstance();
-                        backend.createInitiative(intitiative, new CallbackVoid() {
-                            @Override
-                            public void onSuccess() {
-                                toast_succsess.show();
-                                Intent intent = new Intent(InitiativeActivity.this, MainActivity.class);
-                                startActivity(intent);
-                            }
-
-                            @Override
-                            public void onFailure(String message, Integer httpStatus){
-                                Toast.makeText(InitiativeActivity.this, " No se pudo procesar la operación", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else {
-                        toast_descriptionError.show();
-                    }
-                } else {
-                    toast_photoError.show();
+                if (validateField()) {
+                    Initiative intitiative = new Initiative(textDescription.getText().toString(), bitmap, true);
+                    Backend backend = Backend.getInstance();
+                    backend.createInitiative(intitiative, new CallbackVoid() {
+                        @Override
+                        public void onSuccess() {
+                            Intent intent = new Intent(InitiativeActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(InitiativeActivity.this, "INICIATIVA CREADA", Toast.LENGTH_LONG).show();
+                        }
+                        @Override
+                        public void onFailure(String message, Integer httpStatus) {
+                            Toast.makeText(InitiativeActivity.this, "NO SE PUDO PROCESAR LA OPERACION", Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
             }
         });
+    }
 
+    // valida que los atributos para la creación de la iniciativa sean válidos.
+    private boolean validateField() {
+        Boolean result = true;
+        if (bitmap == null) {
+            result = false;
+            Toast.makeText(InitiativeActivity.this, "DEBE AGREGAR UNA FOTO", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(textDescription.getText())) {
+            result = false;
+            Toast.makeText(InitiativeActivity.this, "DEBE AGREGAR UNA DESCRIPCION", Toast.LENGTH_LONG).show();
+        }
+        return result;
     }
 
     @Override
@@ -120,8 +120,9 @@ public class InitiativeActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1001);
                 setResult(RESULT_OK);
             } else {
-                Toast.makeText(InitiativeActivity.this, " DAME LOS PERMISOS PARA LA CAMARA AMEGO", Toast.LENGTH_SHORT).show();
+                Toast.makeText(InitiativeActivity.this, "ES NECESARIO APROBAR LOS PERMISOS DE LA CAMARA PARA TOMAR LA FOTO", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
 }
