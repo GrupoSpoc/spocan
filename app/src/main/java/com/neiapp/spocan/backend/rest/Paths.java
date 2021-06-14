2598cb4585b1d3779f6474531a7a2bfde506d27b
gaston <gaston.92@hotmail.com.ar>
2021-06-14 10:35:27 -0300

    Descomento PROD

--- app/src/main/java/com/neiapp/spocan/backend/rest/Paths.java
+++ app/src/main/java/com/neiapp/spocan/backend/rest/Paths.java
@@ -3,13 +3,13 @@ package com.neiapp.spocan.backend.rest;
 public class Paths {
 
     // PROD
-    //public static final String BASE = "https://spochi.herokuapp.com";
+    public static final String BASE = "https://spochi.herokuapp.com";
 
     // TEST
     //public static final String BASE = "https://spochi-test.herokuapp.com";
 
     // LOCAL
-    public static final String BASE = "http://10.0.2.2:8080";
+    //public static final String BASE = "http://10.0.2.2:8080";
 
     // Authenticate
     public static final String AUTHENTICATE = "/authenticate";
