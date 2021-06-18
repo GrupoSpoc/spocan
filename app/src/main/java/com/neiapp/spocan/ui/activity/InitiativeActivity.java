667a1af00ae3d49561f41d8329ec8cdf49ad7ce1
lPretti <lprettimolina@gmail.com>
2021-06-18 13:06:33 -0300

    saco import sobrante

--- app/src/main/java/com/neiapp/spocan/ui/activity/InitiativeActivity.java
+++ app/src/main/java/com/neiapp/spocan/ui/activity/InitiativeActivity.java
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
