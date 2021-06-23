33dc7c8f6f6ab8768030c6f64f5997f302667c3f
lPretti <lprettimolina@gmail.com>
2021-06-23 20:04:46 -0300

    arreglo test

--- app/src/test/java/com/neiapp/spocan/ui/activity/InitiativeActivityTest.java
+++ app/src/test/java/com/neiapp/spocan/ui/activity/InitiativeActivityTest.java
@@ -1,7 +1,6 @@
 package com.neiapp.spocan.ui.activity;
 
 import android.graphics.Bitmap;
-import com.neiapp.spocan.backend.MockedImagesBase64;
 import com.neiapp.spocan.util.Base64Converter;
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -16,7 +15,6 @@ import java.util.Scanner;
 import static com.neiapp.spocan.ui.activity.InitiativeActivity.controlImageSize;
 
 
-@Config(manifest= Config.NONE)
 @RunWith(RobolectricTestRunner.class)
 public class InitiativeActivityTest extends TestCase {
 
@@ -43,7 +41,7 @@ public class InitiativeActivityTest extends TestCase {
         Bitmap bmp = Base64Converter.base64ToBitmap(leerBase64());
         final Bitmap resultingBitmap = controlImageSize(bmp);
 
-        assertTrue(bmp.getHeight() != resultingBitmap.getHeight());
-        assertTrue(bmp.getWidth() != resultingBitmap.getWidth());
+        assertTrue(bmp.getHeight() > resultingBitmap.getHeight());
+        assertTrue(bmp.getWidth() > resultingBitmap.getWidth());
     }
 }
